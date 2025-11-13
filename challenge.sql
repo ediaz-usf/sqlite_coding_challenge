c417666c484f010ab0111e26b34f3d22e6c830e1
Ernesto Diaz <diaz37@usf.edu>
2025-11-13 14:36:07 -0500

    task 1 complete

--- challenge.sql
+++ challenge.sql
@@ -1,6 +1,18 @@
 -- task 1
 
-select * from customers;
+SELECT 
+    c.id,
+    first_name,
+    last_name,
+    product_id,
+   sum(quantity * unit_price) AS total_spent
+from customers c
+join orders o on c.id = o.customer_id
+join order_items oi on o.id = oi.order_id
+group by c.id, first_name, last_name
+order by total_spent desc
+limit 5;
+
 
 
 
